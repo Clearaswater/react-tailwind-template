7f312f98d4540eb6297c45b0e2fe26d8d293e71e
Clearaswater <124587154+Clearaswater@users.noreply.github.com>
2024-05-16 14:49:00 +0700

    1page

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 const message = 'This is PowerBI Financial Dashboard';
-const btnText = 'Click me to view the Power BI report';
 
 function App() {
-  const [showReport, setShowReport] = useState(false);
-
-  const handleClick = () => {
-    setShowReport(true);
-  };
-
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-8">{message}</h1>
-      <button
-        className="bg-blue-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={handleClick}
-      >
-        {btnText}
-      </button>
-      {showReport && (
-        <div className="mt-8">
-          <iframe
-            title="try_finance"
-            width="600"
-            height="373.5"
-            src="https://app.powerbi.com/view?r=eyJrIjoiZGVmNzIwNTktY2ZlYy00ZWQwLTg1NTYtYTJlNTU1NzIxNmExIiwidCI6IjZmNDQzMmRjLTIwZDItNDQxZC1iMWRiLWFjMzM4MGJhNjMzZCIsImMiOjEwfQ%3D%3D"
-            frameBorder="0"
-            allowFullScreen={true}
-          ></iframe>
-        </div>
-      )}
+      <div className="mt-8">
+        <iframe
+          title="try_finance"
+          width="600"
+          height="373.5"
+          src="https://app.powerbi.com/view?r=eyJrIjoiZGVmNzIwNTktY2ZlYy00ZWQwLTg1NTYtYTJlNTU1NzIxNmExIiwidCI6IjZmNDQzMmRjLTIwZDItNDQxZC1iMWRiLWFjMzM4MGJhNjMzZCIsImMiOjEwfQ%3D%3D"
+          frameBorder="0"
+          allowFullScreen={true}
+        ></iframe>
+      </div>
     </div>
   );
 }
